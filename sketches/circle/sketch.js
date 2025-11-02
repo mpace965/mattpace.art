427a0399aea8993ff459d3adfade9e4095630dc1
Matt Pace <matthew.dp.96@gmail.com>
2025-11-02 18:36:12 -0500

    replace p5@2.05 with p5@1.11.11 (the types are better in 1.11.11 right now)

--- sketches/circle/sketch.js
+++ sketches/circle/sketch.js
@@ -2,11 +2,11 @@
  * @param {import("./params").Params} params - The sketch parameters
  */
 export function mountSketch(params) {
-  window.setup = function() {
+  globalThis.setup = function() {
     createCanvas(400, 400);
   }
 
-  window.draw = function() {
+  globalThis.draw = function() {
     background(220);
     circle(mouseX, mouseY, params.size)
   }
