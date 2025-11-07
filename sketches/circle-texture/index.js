a6e3784bb123f68c9fd59b27630e80f451fd7ccd
Matt Pace <matthew.dp.96@gmail.com>
2025-11-06 22:26:58 -0500

    reorganize to group things that change together with each other

--- sketches/circle-texture/index.js
+++ sketches/circle-texture/index.js
@@ -1,4 +1,4 @@
-// #region parameters and presets
+// #region sketch
 
 /**
  * @typedef Params
@@ -47,152 +47,24 @@ const PRESETS = {
 
 const DEFAULT_PRESET_NAME = "default";
 
-const PRESET_SEARCH_PARAM = "preset";
-
-function getPresetName() {
-  const url = new URL(window.location.href);
-  const preset = url.searchParams.get(PRESET_SEARCH_PARAM);
-
-  if (preset && isPreset(preset)) {
-    return preset;
-  }
-
-  return DEFAULT_PRESET_NAME;
-}
-
-/**
- * @param {string} preset
- */
-function linkToPreset(preset) {
-  if (isPreset(preset)) {
-    const url = new URL(window.location.href);
-
-    url.searchParams.set(PRESET_SEARCH_PARAM, preset);
-
-    window.location.href = url.toString();
-  } else {
-    throw new Error(
-      `Invalid preset "${preset}", must be one of ${getPresetNames().toString()}.`
-    );
-  }
-}
-
-/**
- * @param {string} preset
- */
-function isPreset(preset) {
-  return preset in PRESETS;
-}
-
-/**
- * @param {string} presetName
- */
-function getParamsForPresetName(presetName) {
-  return PRESETS[presetName];
-}
-
-function getPresetNames() {
-  return Object.keys(PRESETS).sort();
-}
-
-const presetName = getPresetName();
-const params = getParamsForPresetName(presetName);
-
-// #endregion
-
-// #region tweakpane
-
-import { Pane } from "../../vendor/tweakpane@4.0.5/tweakpane.min.js";
-
-/**
- * @param {HTMLElement} paneElement
- */
-function listenForHidePaneEvent(paneElement) {
-  document.addEventListener("keydown", (e) => {
-    if (e.key === "h") {
-      toggleVisibility(paneElement);
-    }
-  });
-}
-
-/**
- * @param {HTMLElement} element
- */
-function toggleVisibility(element) {
-  const currentVisibility = element.style.getPropertyValue("visibility");
-
-  if (currentVisibility !== "hidden") {
-    element.style.setProperty("visibility", "hidden");
-  } else {
-    element.style.setProperty("visibility", "visible");
-  }
-}
-
-/**
- * @param {Pane} pane
- */
-function addSelectPresetDropdown(pane) {
-  const PRESET_PARAMS = {
-    presetName: "",
-  };
-
+function bindParamsToPane(pane, params) {
+  pane.addBinding(params, "circleScale", { min: 0, max: 1 });
   pane
-    .addBinding(PRESET_PARAMS, "presetName", {
-      label: "preset",
-      options: buildPresetOptions(),
-    })
-    .on("change", (ev) => linkToPreset(ev.value));
+    .addBinding(params, "segments", { step: 1, min: 1, max: 200 })
+    .on("change", () => (derivedParams = computeDerivedParams()));
+  pane
+    .addBinding(params, "seed", { step: 1 })
+    .on("change", () => (derivedParams = computeDerivedParams()));
+  pane
+    .addBinding(params, "noiseScale", { min: -0.75, max: 0.75 })
+    .on("change", () => (derivedParams = computeDerivedParams()));
+  pane
+    .addBinding(params, "noiseLod", { step: 1, min: 0 })
+    .on("change", () => (derivedParams = computeDerivedParams()));
+  pane
+    .addBinding(params, "noiseFalloff", { min: 0, max: 1 })
+    .on("change", () => (derivedParams = computeDerivedParams()));
 }
-
-function buildPresetOptions() {
-  const options = {
-    "Select...": "",
-  };
-
-  for (const presetName of getPresetNames()) {
-    options[presetName] = presetName;
-  }
-
-  return options;
-}
-
-/**
- * @param {Pane} pane
- * @param {Params} params
- */
-function addExportPresetButton(pane, params) {
-  const exportBtn = pane.addButton({ title: "Export" });
-  exportBtn.on("click", () =>
-    navigator.clipboard.writeText(JSON.stringify(params))
-  );
-}
-
-const pane = new Pane({ title: `Circle texture ${presetName}` });
-listenForHidePaneEvent(pane.element);
-
-pane.addBinding(params, "circleScale", { min: 0, max: 1 });
-pane
-  .addBinding(params, "segments", { step: 1, min: 1, max: 200 })
-  .on("change", () => (derivedParams = computeDerivedParams()));
-pane
-  .addBinding(params, "seed", { step: 1 })
-  .on("change", () => (derivedParams = computeDerivedParams()));
-pane
-  .addBinding(params, "noiseScale", { min: -0.75, max: 0.75 })
-  .on("change", () => (derivedParams = computeDerivedParams()));
-pane
-  .addBinding(params, "noiseLod", { step: 1, min: 0 })
-  .on("change", () => (derivedParams = computeDerivedParams()));
-pane
-  .addBinding(params, "noiseFalloff", { min: 0, max: 1 })
-  .on("change", () => (derivedParams = computeDerivedParams()));
-
-addSelectPresetDropdown(pane);
-addExportPresetButton(pane, params);
-
-// #endregion
-
-// #region sketch
 
 const CANVAS_SIZE = 800;
 
@@ -221,29 +93,35 @@ function computeDerivedParams() {
   return { values, segmentSize, segmentOffset };
 }
 
-globalThis.setup = function () {
-  createCanvas(CANVAS_SIZE, CANVAS_SIZE);
+function sketch() {
+  globalThis.setup = function () {
+    createCanvas(CANVAS_SIZE, CANVAS_SIZE);
 
-  derivedParams = computeDerivedParams();
-};
+    derivedParams = computeDerivedParams();
+  };
 
-globalThis.draw = function () {
-  background(220);
-  fill(0);
+  globalThis.draw = function () {
+    background(220);
+    fill(0);
 
-  for (let x = 0; x < params.segments; x++) {
-    for (let y = 0; y < params.segments; y++) {
-      diceTexture(
-        x * derivedParams.segmentSize + derivedParams.segmentOffset,
-        y * derivedParams.segmentSize + derivedParams.segmentOffset,
-        derivedParams.segmentSize,
-        derivedParams.values[x][y],
-        params.circleScale,
-        circle
-      );
+    for (let x = 0; x < params.segments; x++) {
+      for (let y = 0; y < params.segments; y++) {
+        diceTexture(
+          x * derivedParams.segmentSize + derivedParams.segmentOffset,
+          y * derivedParams.segmentSize + derivedParams.segmentOffset,
+          derivedParams.segmentSize,
+          derivedParams.values[x][y],
+          params.circleScale,
+          circle
+        );
+      }
     }
-  }
-};
+  };
+}
+
+// #endregion
+
+// #region lib: sketch
 
 function diceTexture(x, y, areaSize, value, scale, callback) {
   value = Math.round(constrain(value, 0, 9));
@@ -346,5 +224,143 @@ function diceTexture(x, y, areaSize, value, scale, callback) {
     callback(x, y, pipSize);
   }
 }
+
+// #endregion
+
+// #region lib: parameters and presets
+
+const PRESET_SEARCH_PARAM = "preset";
+
+function getPresetName() {
+  const url = new URL(window.location.href);
+  const preset = url.searchParams.get(PRESET_SEARCH_PARAM);
+
+  if (preset && isPreset(preset)) {
+    return preset;
+  }
+
+  return DEFAULT_PRESET_NAME;
+}
+
+/**
+ * @param {string} preset
+ */
+function linkToPreset(preset) {
+  if (isPreset(preset)) {
+    const url = new URL(window.location.href);
+
+    url.searchParams.set(PRESET_SEARCH_PARAM, preset);
+
+    window.location.href = url.toString();
+  } else {
+    throw new Error(
+      `Invalid preset "${preset}", must be one of ${getPresetNames().toString()}.`
+    );
+  }
+}
+
+/**
+ * @param {string} preset
+ */
+function isPreset(preset) {
+  return preset in PRESETS;
+}
+
+/**
+ * @param {string} presetName
+ */
+function getParamsForPresetName(presetName) {
+  return PRESETS[presetName];
+}
+
+function getPresetNames() {
+  return Object.keys(PRESETS).sort();
+}
+
+// #endregion
+
+// #region lib: tweakpane
+
+import { Pane } from "../../vendor/tweakpane@4.0.5/tweakpane.min.js";
+
+/**
+ * @param {HTMLElement} paneElement
+ */
+function listenForHidePaneEvent(paneElement) {
+  document.addEventListener("keydown", (e) => {
+    if (e.key === "h") {
+      toggleVisibility(paneElement);
+    }
+  });
+}
+
+/**
+ * @param {HTMLElement} element
+ */
+function toggleVisibility(element) {
+  const currentVisibility = element.style.getPropertyValue("visibility");
+
+  if (currentVisibility !== "hidden") {
+    element.style.setProperty("visibility", "hidden");
+  } else {
+    element.style.setProperty("visibility", "visible");
+  }
+}
+
+/**
+ * @param {Pane} pane
+ */
+function addSelectPresetDropdown(pane) {
+  const PRESET_PARAMS = {
+    presetName: "",
+  };
+
+  pane
+    .addBinding(PRESET_PARAMS, "presetName", {
+      label: "preset",
+      options: buildPresetOptions(),
+    })
+    .on("change", (ev) => linkToPreset(ev.value));
+}
+
+function buildPresetOptions() {
+  const options = {
+    "Select...": "",
+  };
+
+  for (const presetName of getPresetNames()) {
+    options[presetName] = presetName;
+  }
+
+  return options;
+}
+
+/**
+ * @param {Pane} pane
+ * @param {Params} params
+ */
+function addExportPresetButton(pane, params) {
+  const exportBtn = pane.addButton({ title: "Export" });
+  exportBtn.on("click", () =>
+    navigator.clipboard.writeText(JSON.stringify(params))
+  );
+}
+
+// #endregion
+
+// #region bootstrap
+
+const presetName = getPresetName();
+const params = getParamsForPresetName(presetName);
+
+const pane = new Pane({ title: `Circle texture ${presetName}` });
+listenForHidePaneEvent(pane.element);
+
+bindParamsToPane(pane, params);
+
+addSelectPresetDropdown(pane);
+addExportPresetButton(pane, params);
+
+sketch();
 
 // #endregion
